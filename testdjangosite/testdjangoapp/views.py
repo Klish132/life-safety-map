88980d6cfef710b0babb7a24e60d265ae6fce35e
Klish132 <tuha99@mail.ru>
2019-12-10 23:41:26 +0500

    Finished markers

--- testdjangosite/testdjangoapp/views.py
+++ testdjangosite/testdjangoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import os
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,5 +11,9 @@ def show(request):
     # found in the Mapbox account settings and getting started instructions
     # see https://www.mapbox.com/account/ under the "Access tokens" section
     mapbox_access_token = 'pk.my_mapbox_access_token'
+    path = "testdjangoapp\\templates\\points"
+    with open("testdjangoapp\\templates\\points\\point_1.txt", "r") as file:
+        strlist = json.dumps(file.readlines())
     return render(request, 'page1.html',
+                  { 'liststr' : strlist },
                   { 'mapbox_access_token': mapbox_access_token })
